7bc9a8572d03144ef8b3bbb84e55f4fea2e30e4c
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-05-02 11:15:01 +0200

    header responsive

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
+"use strict";
+
 const hamburgerIcon = document.querySelector(".header__hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
+const navLitems = document.querySelectorAll(".nav-menu__litem");
 
 hamburgerIcon.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu--show");
@@ -8,4 +11,25 @@ hamburgerIcon.addEventListener("click", () => {
   } else {
     hamburgerIcon.src = "/images/icon-hamburger.svg";
   }
+});
+
+//Closes all the Sublist items in the nav bar menu
+const closeSubLists = function () {
+  navLitems.forEach((item) => {
+    item.nextElementSibling.classList.remove("nav-menu--show");
+  });
+};
+
+//check for each click event from the menu items for display the sublist
+navLitems.forEach((item) => {
+  item.addEventListener("click", () => {
+    //get the sublistclass by clicked item nextelementsibling
+    const subListClass = item.nextElementSibling.classList;
+    const subListShown = subListClass.contains("nav-menu--show");
+    if (subListShown) subListClass.remove("nav-menu--show");
+    else {
+      closeSubLists();
+      subListClass.add("nav-menu--show");
+    }
+  });
 });
